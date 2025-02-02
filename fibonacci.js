026da7efa864642903b939b75d67110c0c7a6892
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-02-02 15:17:14 -0500

    MergeSort function done

--- fibonacci.js
+++ fibonacci.js
@@ -1,134 +1,138 @@
-// console.log(factorial(5))
-
-function sumRange(num) {
-  if (num == 1) return 1
-  return num + sumRange(num - 1)
-}
-
-function power(base, exp) {
-  if (exp == 0) return 1
-  return base * power(base, exp - 1)
-}
-
-function factorial(n) {
-  if (n === 1 || n === 0) {
-    return 1
+function fibs(n, arr = []) {
+  let a = 0
+  let b = 1
+  let count = 0
+  while (count < n) {
+    arr.push(a)
+    let c = a + b
+    a = b
+    b = c
+    count++
   }
-  return n * factorial(n - 1)
+  return arr
 }
 
-function all(array, callback) {
-  if (array.length === 0) {
-    return true
+function fibsRec(n, a = 0, b = 1, count = 0, arr = []) {
+  if (count === n) {
+    return arr
   }
-
-  if (array.length === 1) {
-    return callback(array[0])
-  }
-
-  return callback(array[0]) && all(array.slice(1), callback)
+  arr.push(a)
+  return fibsRec(n, b, a + b, count + 1, arr)
 }
 
-const allAreLessThanSeven = all([4, 3, 2], function (num) {
-  return num < 7
-})
+console.log(fibs(24))
+console.log(fibsRec(24))
 
-//  if (num == 1) return 1
-//return num + sumRange(num - 1)
-
-function productOfArray(array) {
-  if (array.length === 0) {
-    return 1
+/* JS Interview Prep (My solutions)
+    function sumRange(num) {
+    if (num == 1) return 1
+    return num + sumRange(num - 1)
   }
-  return array.shift() * productOfArray(array)
-}
-// console.log(productOfArray([1, 2, 3, 10]))
 
-var nestedObject = {
-  data: {
-    info: {
-      stuff: {
-        thing: {
-          moreStuff: {
-            magicNumber: 44,
-            something: "foo2",
+  function power(base, exp) {
+    if (exp == 0) return 1
+    return base * power(base, exp - 1)
+  }
+
+  function factorial(n) {
+    if (n === 1 || n === 0) {
+      return 1
+    }
+    return n * factorial(n - 1)
+  }
+
+  function all(array, callback) {
+    if (array.length === 0) {
+      return true
+    }
+
+    if (array.length === 1) {
+      return callback(array[0])
+    }
+
+    return callback(array[0]) && all(array.slice(1), callback)
+  }
+
+  const allAreLessThanSeven = all([4, 3, 2], function (num) {
+    return num < 7
+  })
+
+  function productOfArray(array) {
+    if (array.length === 0) {
+      return 1
+    }
+    return array.shift() * productOfArray(array)
+  }
+
+  var nestedObject = {
+    data: {
+      info: {
+        stuff: {
+          thing: {
+            moreStuff: {
+              magicNumber: 44,
+              something: "foo2",
+            },
           },
         },
       },
     },
-  },
-}
+  }
 
-function contains(nestedObject, value) {
-  for (var prop in nestedObject) {
-    if (Object.prototype.hasOwnProperty.call(nestedObject, prop)) {
-      if (
-        typeof nestedObject[prop] === "object" &&
-        nestedObject[prop] !== null
-      ) {
-        return contains(nestedObject[prop], value)
+  function contains(nestedObject, value) {
+    for (var prop in nestedObject) {
+      if (Object.prototype.hasOwnProperty.call(nestedObject, prop)) {
+        if (
+          typeof nestedObject[prop] === "object" &&
+          nestedObject[prop] !== null
+        ) {
+          return contains(nestedObject[prop], value)
+        }
+
+        if (nestedObject[prop] === value) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  function totalIntegers(multiDimensionalArray, total = 0) {
+    for (let int of multiDimensionalArray) {
+      if (Array.isArray(int)) {
+        total = totalIntegers(int, total)
       }
 
-      if (nestedObject[prop] === value) {
-        return true
+      if (typeof int === "number") {
+        total++
       }
     }
+    return total
   }
-  return false
-}
-// console.log(contains(nestedObject, 44)) // true
-// console.log(contains(nestedObject, "foo"))
 
-function totalIntegers(multiDimensionalArray, total = 0) {
-  for (let int of multiDimensionalArray) {
-    if (Array.isArray(int)) {
-      total = totalIntegers(int, total)
+  function SumSquares(array, total = 0) {
+    for (let int of array) {
+      if (Array.isArray(int)) {
+        total = SumSquares(int, total)
+      }
+
+      if (typeof int === "number") {
+        total += power(int, 2)
+      }
+    }
+    return total
+  }
+
+  function replicate(repetitions, num, arr = []) {
+    if (repetitions < 0) {
+      return []
     }
 
-    if (typeof int === "number") {
-      total++
+    if (repetitions === 0) {
+      return arr
     }
-  }
-  return total
-}
-
-function SumSquares(array, total = 0) {
-  for (let int of array) {
-    if (Array.isArray(int)) {
-      total = SumSquares(int, total)
-    }
-
-    if (typeof int === "number") {
-      total += power(int, 2)
-    }
-  }
-  return total
-}
-
-// console.log(totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]))
-// var l = [1, 2, 3]
-// console.log(SumSquares(l)) // 1 + 4 + 9 = 14
-
-// l = [[1, 2], 3]
-// console.log(SumSquares(l)) // 1 + 4 + 9 = 14
-
-// l = [[[[[[[[[1]]]]]]]]]
-// console.log(SumSquares(l)) // 1 = 1
-
-// l = [10, [[10], 10], [10]]
-// console.log(SumSquares(l)) // 100 + 100 + 100 + 100 = 400
-
-function replicate(repetitions, num, arr = []) {
-  if (repetitions < 0) {
-    return []
+    arr.push(num)
+    return replicate(repetitions - 1, num, arr)
   }
 
-  if (repetitions === 0) {
-    return arr
-  }
-  arr.push(num)
-  return replicate(repetitions - 1, num, arr)
-}
-console.log(replicate(3, 5)) // [5, 5, 5]
-console.log(replicate(1, 69)) // [69]
-console.log(replicate(-2, 6)) // []
+*/
