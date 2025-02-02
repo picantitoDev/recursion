9ff4fc98f31966ec0dc61436cba07fd7f4432bdb
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-02-01 20:44:41 -0500

    fact function done

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-console.log(power(2, 3))
+console.log(factorial(5))
 
 function sumRange(num) {
   if (num == 1) return 1
@@ -10,9 +10,16 @@ function power(base, exp) {
   return base * power(base, exp - 1)
 }
 
+function factorial(n) {
+  if (n === 1 || n === 0) {
+    return 1
+  }
+  return n * factorial(n - 1)
+}
+
 /*
-sumRange(2, 3) → 2 * power(2, 3-1) 
-sumRange(2, 2) → 2 * power(2, 2-1)
-sumRange(2, 1) → 2 * power(2, 1-1)
-sumRange(2, 0) → 1 
+fact(4) → 4 * fact(3) 
+fact() → 2 * power(2, 3-1) 
+fact() → 2 * power(2, 2-1)
+fact() → 2 * power(2, 1-1)
 */
